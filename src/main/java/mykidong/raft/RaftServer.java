7e147d22e5177428d678ae338c4ba6f33c2f56c1
Kidong Lee <mykidong@gmail.com>
2020-05-21 13:03:44 +0900

    .

--- src/main/java/mykidong/raft/RaftServer.java
+++ src/main/java/mykidong/raft/RaftServer.java
@@ -32,7 +32,7 @@ public class RaftServer {
         BlockingQueue<Request> requestQueue = new LinkedBlockingQueue<>();
 
         // poll timeout for request queue.
-        long requestQueuePollTimeout = 1000;
+        long requestQueuePollTimeout = 10;
 
         // channel processors.
         List<ChannelProcessor> channelProcessors = new ArrayList<>();
