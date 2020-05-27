492ccf479b2625948bb2842e404ab1cdc47b3c4b
Kidong Lee <mykidong@gmail.com>
2020-05-27 14:09:13 +0900

    socket client added...

--- src/main/java/mykidong/raft/client/SocketClient.java
+++ src/main/java/mykidong/raft/client/SocketClient.java
@@ -6,14 +6,8 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.ClosedSelectorException;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.Iterator;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 public class SocketClient extends Thread {
 
@@ -22,36 +16,77 @@ public class SocketClient extends Thread {
     private Selector selector;
     private String host;
     private int port;
-    private BlockingQueue<ByteBuffer> requestBufferQueue;
-    private BlockingQueue<ByteBuffer> responseBufferQueue;
+    private SocketChannel socketChannel;
+    private boolean ready = false;
 
     public SocketClient(String host, int port) {
         this.host = host;
         this.port = port;
-        this.requestBufferQueue = new LinkedBlockingQueue<>(1);
-        this.responseBufferQueue = new LinkedBlockingQueue<>(1);
     }
 
-    public ByteBuffer sendMessage(ByteBuffer request) {
-        ByteBuffer response = null;
-        try {
-            this.requestBufferQueue.put(request);
-            while(true) {
-                if(this.responseBufferQueue.isEmpty()) {
-                    response = this.responseBufferQueue.poll(10, TimeUnit.MICROSECONDS);
-                } else {
-                    response = this.responseBufferQueue.poll();
-                }
-
-                if(response != null) {
-                    break;
-                }
+    public ByteBuffer sendMessage(ByteBuffer requestBuffer) {
+        while (!ready)
+        {
+            LOG.info("client not ready...");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                LOG.error(e.getMessage());
             }
-        } catch (InterruptedException e) {
+        }
+
+        // send request message.
+        send(requestBuffer);
+
+        // get response message.
+        ByteBuffer responseBuffer = receive();
+
+        return responseBuffer;
+    }
+
+    private ByteBuffer receive() {
+        try {
+            // switch to read.
+            socketChannel.register(this.selector, SelectionKey.OP_READ);
+
+            // to get total size.
+            ByteBuffer totalSizeBuffer = ByteBuffer.allocate(4);
+            socketChannel.read(totalSizeBuffer);
+
+            totalSizeBuffer.rewind();
+
+            // total size.
+            int totalSize = totalSizeBuffer.getInt();
+
+            // subsequent bytes buffer.
+            ByteBuffer buffer = ByteBuffer.allocate(totalSize);
+            socketChannel.read(buffer);
+
+            buffer.rewind();
+
+            return buffer;
+        } catch (ClosedChannelException e) {
+            LOG.error(e.getMessage());
+        } catch (IOException e) {
             LOG.error(e.getMessage());
         }
 
-        return response;
+        return null;
+    }
+
+    private void send(ByteBuffer request) {
+        try {
+            // switch to write.
+            socketChannel.register(this.selector, SelectionKey.OP_WRITE);
+
+            while (request.hasRemaining()) {
+                socketChannel.write(request);
+            }
+        } catch (ClosedChannelException e) {
+            LOG.error(e.getMessage());
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+        }
     }
 
     private void connect() {
@@ -70,6 +105,9 @@ public class SocketClient extends Thread {
     public void run() {
         connect();
 
+        // client is ready to request.
+        this.ready = true;
+
         while (true) {
             try {
                 int readyChannels = selector.select();
@@ -87,7 +125,6 @@ public class SocketClient extends Thread {
             Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
             while (iter.hasNext()) {
                 SelectionKey key = iter.next();
-
                 iter.remove();
 
                 if (!key.isValid()) {
@@ -96,10 +133,6 @@ public class SocketClient extends Thread {
 
                 if (key.isConnectable()) {
                     this.connect(key);
-                } else if (key.isReadable()) {
-                    this.readBytes(key);
-                } else if (key.isWritable()) {
-                    this.writeBytes(key);
                 }
             }
         }
@@ -107,7 +140,7 @@ public class SocketClient extends Thread {
 
     private void connect(SelectionKey key) {
         try {
-            SocketChannel socketChannel = (SocketChannel) key.channel();
+            this.socketChannel = (SocketChannel) key.channel();
             socketChannel.configureBlocking(false);
 
             if (socketChannel.isConnectionPending()) {
@@ -120,57 +153,6 @@ public class SocketClient extends Thread {
 
             // switch to write.
             socketChannel.register(this.selector, SelectionKey.OP_WRITE);
-        } catch (IOException e) {
-            LOG.error(e.getMessage());
-        }
-    }
-
-    private void readBytes(SelectionKey key) {
-        try {
-            SocketChannel socketChannel = (SocketChannel) key.channel();
-
-            // to get total size.
-            ByteBuffer totalSizeBuffer = ByteBuffer.allocate(4);
-            socketChannel.read(totalSizeBuffer);
-
-            totalSizeBuffer.rewind();
-
-            // total size.
-            int totalSize = totalSizeBuffer.getInt();
-
-            // subsequent bytes buffer.
-            ByteBuffer buffer = ByteBuffer.allocate(totalSize);
-            socketChannel.read(buffer);
-
-            buffer.rewind();
-
-            LOG.info("response: [{}]", new String(buffer.array()));
-
-            // switch to write.
-            socketChannel.register(this.selector, SelectionKey.OP_WRITE);
-        } catch (IOException e) {
-            LOG.error(e.getMessage());
-        }
-    }
-
-    private void writeBytes(SelectionKey key) {
-        try {
-            SocketChannel socketChannel = (SocketChannel) key.channel();
-
-            byte[] messageBytes = new String("this is client request...").getBytes();
-            int messageLength = messageBytes.length;
-            ByteBuffer buffer = ByteBuffer.allocate(4 + messageLength);
-            buffer.putInt(messageBytes.length);
-            buffer.put(messageBytes);
-
-            buffer.rewind();
-
-            while (buffer.hasRemaining()) {
-                socketChannel.write(buffer);
-            }
-
-            // switch to read.
-            socketChannel.register(this.selector, SelectionKey.OP_READ);
         } catch (IOException e) {
             LOG.error(e.getMessage());
         }
--- src/test/java/mykidong/raft/RunClient.java
+++ src/test/java/mykidong/raft/RunClient.java
@@ -4,8 +4,21 @@ import mykidong.raft.client.SocketClient;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class RunClient {
+
+    private static Logger LOG = LoggerFactory.getLogger(RunClient.class);
 
     @Before
     public void init() throws Exception {
@@ -14,12 +27,74 @@ public class RunClient {
     }
 
     @Test
-    public void run() throws Exception {
+    public void runMultipleClients() throws Exception {
+        ExecutorService executor = Executors.newFixedThreadPool(50);
+        List<Future<String>> futureList = new ArrayList<Future<String>>();
+
+        long start = System.currentTimeMillis();
+
+        int TASK_MAX = 20;
+        int messageCount = 100000;
+        for(int i = 0; i < TASK_MAX; i++) {
+            Future<String> future = executor.submit(() -> {
+                return sendSimpleMessages(messageCount);
+            });
+            futureList.add(future);
+        }
+
+        for (Future<String> fut : futureList) {
+            try {
+                String result = new Date() + "::" + fut.get();
+                LOG.info(result);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        LOG.info("tps all: [{}]", ((double)(TASK_MAX * messageCount) / (double)(System.currentTimeMillis() - start)) * 1000);
+
+        executor.shutdown();
+    }
+
+    @Test
+    public void runSingleClient() throws Exception {
+        String tps = sendSimpleMessages(1000);
+        LOG.info("tps: [{}]", tps);
+    }
+
+    private static String sendSimpleMessages(int count) {
+
         String host = "localhost";
         int port = 9912;
         SocketClient client = new SocketClient(host, port);
         client.start();
 
-        Thread.sleep(Long.MAX_VALUE);
+        long start = System.currentTimeMillis();
+        int MAX = count;
+        for(int i = 0; i < MAX; i++) {
+            ByteBuffer buffer = buildRequest(i);
+
+            ByteBuffer responseBuffer = client.sendMessage(buffer);
+            printResponse(responseBuffer);
+        }
+        String tps = "tps: " + ((double)MAX / (double)(System.currentTimeMillis() - start)) * 1000;
+
+        return tps;
+    }
+
+    private static ByteBuffer buildRequest(int i) {
+        byte[] messageBytes = new String("this is client request..." + i).getBytes();
+        int messageLength = messageBytes.length;
+        ByteBuffer buffer = ByteBuffer.allocate(4 + messageLength);
+        buffer.putInt(messageBytes.length);
+        buffer.put(messageBytes);
+        buffer.flip();
+
+        return buffer;
+    }
+
+    private static void printResponse(ByteBuffer buffer) {
+        buffer.rewind();
+        LOG.debug("response: [{}]", new String(buffer.array()));
     }
 }
